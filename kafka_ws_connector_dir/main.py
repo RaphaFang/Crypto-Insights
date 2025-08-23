d9655c0bb88775edc1d90728fa0f15de1c336ada
方思渝 <fangsihyu0211@gmail.com>
2025-08-23 22:11:34 +0200

    add: .encode() for transfer into kafka

--- kafka_ws_connector_dir/main.py
+++ kafka_ws_connector_dir/main.py
@@ -1,9 +1,14 @@
 import os, uvloop
+from ws_component import binance_trades
+
 
 WS_URL = os.getenv("WS_URL", "wss://stream.binance.com:9443/ws/btcusdt@trade")
 KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP", "kafka:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "ws_raw")
 
+
+async def main():
+    await binance_trades(WS_URL)
 
 if __name__ == "__main__":
     uvloop.run(main())
--- kafka_ws_connector_dir/ws_component.py
+++ kafka_ws_connector_dir/ws_component.py
@@ -1,7 +1,5 @@
 import os, json, uvloop, websockets
 
-WS_URL = os.getenv("WS_URL", "wss://stream.binance.com:9443/ws/btcusdt@trade")
-
 async def binance_trades(ws_url: str):
     async with websockets.connect(
         ws_url,
@@ -11,16 +9,11 @@ async def binance_trades(ws_url: str):
     ) as websocket:
         while True:
             try:
-                data = await websocket.recv()     # 文字訊息
-                trade = json.loads(data)          # 轉成 dict
-                print(trade)                      # 先用 print，之後再換 logging
+                data = await websocket.recv()
+                print(data.encode("utf-8"))
             except Exception as e:
                 print("ERROR:", e)
                 break
 
-async def main():
-    await binance_trades(WS_URL)
 
-if __name__ == "__main__":
-    uvloop.run(main())
 
