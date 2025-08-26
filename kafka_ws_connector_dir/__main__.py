a1c1f3f8e20860caab43c20832453f7281cd05ff
方思渝 <fangsihyu0211@gmail.com>
2025-08-26 16:03:54 +0200

    add: functionable ws to kafka connector, and it's dockerfile.

---
+++ kafka_ws_connector_dir/__main__.py
@@ -0,0 +1,26 @@
+import os, uvloop
+from .k_producer import make_producer, send, close
+from .ws_component import binance_trades
+from .logger import setup_logger
+
+
+WS_URL = os.getenv("WS_URL", "wss://stream.binance.com:9443/ws/btcusdt@trade")
+KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP", "kafka:9092")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "btc_raw")
+LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
+
+async def main():
+    p = make_producer(KAFKA_BOOTSTRAP)
+    try:
+        async for msg in binance_trades(WS_URL):
+            send(p=p, topic=KAFKA_TOPIC, value=msg.encode("utf-8"))
+    finally:
+        close(p)
+
+setup_logger(LEVEL)
+uvloop.run(main())
+
+# if __name__ == "__main__":
+#     setup_logger(LEVEL)
+#     uvloop.run(main())
