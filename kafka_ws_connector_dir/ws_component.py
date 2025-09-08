08a9873fdcd37f59a97bd8803855855fbaf5d39a
方思渝 <fangsihyu0211@gmail.com>
2025-09-08 13:07:04 +0200

    add: grafana comtainer setup

--- kafka_ws_connector_dir/ws_component.py
+++ kafka_ws_connector_dir/ws_component.py
@@ -5,6 +5,7 @@ log = logging.getLogger("ws")
 async def binance_trades(ws_url: str):
     async with websockets.connect(
         ws_url,
+        open_timeout=30,
         ping_interval=20,
         ping_timeout=20,
         close_timeout=10,) as ws:
@@ -17,4 +18,3 @@ async def binance_trades(ws_url: str):
             except Exception as e:
                 log.error("ERROR:", e)
                 break
-
