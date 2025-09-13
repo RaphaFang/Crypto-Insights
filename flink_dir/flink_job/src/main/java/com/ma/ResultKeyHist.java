639c6be78b2b664f8d7c90a3b8795eee99c5d3ee
方思渝 <fangsihyu0211@gmail.com>
2025-09-13 15:19:44 +0200

    chore: healthcheck for container init-clickhouse lunching

--- flink_dir/flink_job/src/main/java/com/ma/ResultKeyHist.java
+++ flink_dir/flink_job/src/main/java/com/ma/ResultKeyHist.java
@@ -7,7 +7,10 @@ import org.apache.flink.api.common.serialization.SerializationSchema;
 class ResultKeyHist implements SerializationSchema<VwapResult> {
   @Override
   public byte[] serialize(VwapResult r) {
-    String key = r.symbol + "|" + r.ma + "|" + r.windowEnd;
+    String key = r.symbol + "|" + r.ma;
     return key.getBytes(StandardCharsets.UTF_8);
   }
 }
+// + "|" + r.windowEnd
+// 我先前在這邊加上這段，會導致美筆資料都不同key，導致大家分區都變得隨機了
+// Sum up, 如果是同樣的 key ，一定會在同一區。但是不同key，也可能到你那區或是不同區。
